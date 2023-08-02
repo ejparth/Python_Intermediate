d2b36925815e25c1fe4fd8464ef6161a28576186
aakash.saxena08@gmail.com <oceanboys@123>
2023-08-02 20:08:10 +0530

    Initial commit

---
+++ Details.py
@@ -0,0 +1,9 @@
+class A:
+    "My name is Akash"  # It should be on first line, called through __doc__ attribute
+
+    def __init__(self):
+        self.age = 10
+
+    def fun(self):
+        self.name = "Let's learn coding"
+        print(self.name)
---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a practice session for classes in python
+import Details
+
+# importing class
+"-----------------------------------------------------------------------------------"
+
+obj = Details.A()  # Calling Class
+print(obj.age)  # Calling variable in class
+
+"-----------------------------------------------------------------------------------"
+
+print(obj.__doc__)  # Print document string in class
+
+"-----------------------------------------------------------------------------------"
+
+print(obj.fun())
